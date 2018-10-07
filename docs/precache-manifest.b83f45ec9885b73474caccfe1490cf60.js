02615ae3d793ab1568b8f75c8fedafbae7f7424c
Veronica Bautista <veronicabautista87@gmail.com>
2018-10-07 16:29:58 +0200

    add github pages

---
+++ docs/precache-manifest.b83f45ec9885b73474caccfe1490cf60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb10e5f1376de4ba6667173d6ec0b219",
+    "url": "./static/media/pikachu-bg.bb10e5f1.png"
+  },
+  {
+    "revision": "799db9c7d18bf8e9f25e906760535649",
+    "url": "./static/media/logo.799db9c7.png"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "e6dedd7a76db0b9aabc1",
+    "url": "./static/js/main.e6dedd7a.chunk.js"
+  },
+  {
+    "revision": "7268bd7543b576ee0803",
+    "url": "./static/js/1.7268bd75.chunk.js"
+  },
+  {
+    "revision": "e6dedd7a76db0b9aabc1",
+    "url": "./static/css/main.da3ab013.chunk.css"
+  },
+  {
+    "revision": "235660d63254e152aef73c29e13f4e38",
+    "url": "./index.html"
+  }
+];
