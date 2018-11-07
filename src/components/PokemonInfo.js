dfe487b63d2238a5f02c6860a2c4a3ae8a725f25
Veronica Bautista <veronicabautista87@gmail.com>
2018-11-07 19:06:41 +0100

    add grid structure

--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -12,34 +12,35 @@ class PokemonInfo extends React.Component {
     return (
       <div className="pokemon__info--card">
       <p className="pokemon__info--name">{name}</p>
-          <img className="pokemon__info--picture" src={sprites.front_default} alt="pokemon" />
-          <ul className="pokemon__info--attack--type">{types.map((attack, i) => {
-            return <li className={`pokemon__attack type--${types[i].type.name.toLowerCase()}`} key={i}>{attack.type.name}</li>
-            })}
+        <div className="pokemon__info--picture">
+          <img  src={sprites.front_default} alt="pokemon" />
+        </div>
+        <ul className="pokemon__info--attack--type">{types.map((attack, i) => {
+          return <li className={`pokemon__attack type--${types[i].type.name.toLowerCase()}`} key={i}>{attack.type.name}</li>
+          })}
+        </ul>
+        <div className="pokemon__info--details">
+          <p className="pokemon__height">Height: {height} cm</p>
+          <p className="pokemon__weight">Weight: {weight} g</p>
+          <ul className="pokemon__info--abilities">
+            <p>Abilities:</p>
+            {abilities
+              .map((items) => {
+                return (<li className="pokemon__abilities">{items.ability.name}</li>)
+              })
+            }
           </ul>
-          {/* <span className="pokemon__id--container"> */}
-            <p className="pokemon__height">Height: {height} cm</p>
-            <p className="pokemon__weight">Weight: {weight} g</p>
-          
-          <ul className="pokemon__abilities">
-                <p>Abilities:</p>
-                {abilities
-                  .map((items) => {
-                    return (<li className>{items.ability.name}</li>)
-                  })
-                }
-          </ul>
-          {/* <div>
-              <h2 className>Evolutions</h2>
-              <ul className>
-                  {evolutions.map(evolution => {
-                      return (
-                          <p className>{evolution}</p>
-                      );
-                  })}
-              </ul>
-          </div> */}
-          {/* </span> */}
+        {/* <div>
+            <h2 className>Evolutions</h2>
+            <ul className>
+                {evolutions.map(evolution => {
+                    return (
+                        <p className>{evolution}</p>
+                    );
+                })}
+            </ul>
+        </div> */}
+        </div>
       </div>
     )
 }}}
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -17,12 +17,8 @@ class PokemonList extends React.Component {
 								<Link to={`/PokemonInfo/${card.id}`}>
 								<li className="pokemonlist__card" key={card.id}>
 									<PokemonCard 
-									 card={card}
-									//  id={card.id}
-									//  name={card.name}
-									//  types={card.types.map((t) => t.type.name)}
-									//  abilities={card.abilities.map((ability) => ability.ability.name)}
-									 />
+										card={card}
+										/>
 								</li>
 								</Link>
 							)}
